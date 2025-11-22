255832dc7b1b5dd3b6d7095c5c8f1ef10fe481eb
madzone007 <dareboy006@gmail.com>
2025-11-22 20:45:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function createBot() {
   console.log(`🔗 Connection attempt #${connectionAttempts} - This will AUTO-START the server!`);
   
   bot = mineflayer.createBot({
-    host: '49.12.82.39',
-    port: 25852,
+    host: 'madlevel.minekeep.gg',
+    port: 25565,
     username: 'AutoStartBot',
     version: '1.21', // Match your server version
     checkTimeoutInterval: 60 * 1000, // Longer timeout for server start
